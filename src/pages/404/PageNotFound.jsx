6dd2d75e64df1829ef690c88b95bf5148fb899d3
suraj-858 <sooraz858@gmail.com>
2023-05-01 23:20:12 +0545

    movix code completed

---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.scss"
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <div className="NotFound">PageNotFound</div>
+    </div>
+  )
+}
+
+export default PageNotFound
